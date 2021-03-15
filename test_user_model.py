47481d96c270546a9517c1710b5a7338885db4e5
gmzi <gastonmazieres@gmail.com>
2021-03-15 16:30:37 -0400

    tests setup

--- test_user_model.py
+++ test_user_model.py
@@ -5,6 +5,7 @@
 #    python -m unittest test_user_model.py
 
 
+from app import app
 import os
 from unittest import TestCase
 
@@ -20,7 +21,6 @@ os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 
 # Now we can import app
 
-from app import app
 
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
